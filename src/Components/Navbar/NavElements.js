cd90bb5c353fc1a24a494e75d3b5f2f3c9168e66
manshad <manshadchangampalli010@gmail.com>
2022-01-06 15:04:09 +0530

    #ADDED: the sidenavbar is finished

--- src/Components/Navbar/NavElements.js
+++ src/Components/Navbar/NavElements.js
@@ -1,12 +1,14 @@
 import styled from "styled-components";
+var mainColor = "#FFEBB8"
+var whiteColor = "#FFFFFF"
 
 export const Nav = styled.nav`
     width: 60px;
-    height: 400px;
-    background: #000;
+    height: 340px;
+    background: ${whiteColor};
     position: fixed;
     left: 0;
-    padding: 10px 5px;
+    padding: 30px 5px;
     top: 50%;
     transform: translate(0,-50%);
     border-radius:0 40px 40px 0;
@@ -15,7 +17,7 @@ export const NavRoundTop = styled.div`
     position: absolute;
     top: -20px;
     left: 0;
-    background: #000;
+    background: ${whiteColor};
     width: 40px;
     height: 20px;
     &::before{
@@ -25,7 +27,7 @@ export const NavRoundTop = styled.div`
         top: 0;
         position: absolute;
         content:'';
-        background-color:white ;
+        background-color:${mainColor}; ;
         border-radius: 0 0 0 40px;
     }
 `
@@ -33,7 +35,7 @@ export const NavRoundBottom = styled.div`
     position: absolute;
     bottom: -20px;
     left: 0;
-    background: #000;
+    background: ${whiteColor};
     width: 40px;
     height: 20px;
     &::before{
@@ -43,7 +45,7 @@ export const NavRoundBottom = styled.div`
         top: 0;
         position: absolute;
         content: '';
-        background-color:white;
+        background-color:${mainColor};
         border-radius:  40px 0 0 0;
     }
 `
@@ -52,4 +54,10 @@ export const NavItems = styled.div`
     height: 100%;
     display: grid;
     grid-template-rows: repeat(4,1fr);
+`
+export const NavItem = styled.div`
+    width: 100%;
+    height: 100%;
+    display: grid;
+    place-items: center;
 `
--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,12 +1,29 @@
 import React from "react";
-import { Nav, NavItems, NavRoundBottom, NavRoundTop } from "./NavElements";
+import { Nav, NavItem, NavItems, NavRoundBottom, NavRoundTop } from "./NavElements";
+import HomeImg from '../../Assets/images/navImages/Home.png'
+import ProjectImg from '../../Assets/images/navImages/Projects.png'
+import CodeImg from '../../Assets/images/navImages/code.png'
+import Contact from '../../Assets/images/navImages/contact.png'
 
 function NavBar() {
   return (
     <Nav>
       <NavRoundTop />
       <NavRoundBottom />
-      <NavItems/>
+      <NavItems>
+          <NavItem>
+            <img src={HomeImg} alt="" />
+          </NavItem>
+          <NavItem>
+            <img src={ProjectImg} alt="" />
+          </NavItem>
+          <NavItem>
+            <img src={CodeImg} alt="" />
+          </NavItem>
+          <NavItem>
+            <img src={Contact} alt="" />
+          </NavItem>
+      </NavItems>
     </Nav>
   );
 }
